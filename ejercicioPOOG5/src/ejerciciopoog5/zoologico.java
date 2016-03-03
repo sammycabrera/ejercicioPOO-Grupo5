a531bc16132e6f8602717a891ff896f23ae278b2
Administrador <Administrador@10.75.70.241>
2016-03-02 21:02:48 -0500

    inicio del diseño

---
+++ ejercicioPOOG5/src/ejerciciopoog5/zoologico.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopoog5;
+
+/**
+ *
+ * @author Administrador
+ */
+public class zoologico {
+    
+    /**
+     * id del zoologico
+     */
+    
+    private String id_zoologico;
+    
+    /**
+     * id del nombre zoologico
+     */
+    
+    private String nombre;
+    
+    
+    
+    
+    
+    
+}
